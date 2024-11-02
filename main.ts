d8e37363d0ff3adc817a50aa11be608b91e568b3
pceguia <portereguia@gmail.com>
2024-11-02 12:17:15 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,65 @@
+input.onButtonPressed(Button.A, function () {
+    game.pause()
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        `)
+    if (sprite.get(LedSpriteProperty.X) == 0 && _false == just_scored) {
+        just_scored = _true
+        game.addScore(1)
+        basic.showIcon(IconNames.Yes)
+        playScoreSong()
+    } else {
+        playMissSong()
+    }
+    game.resume()
+})
+function playMissSong () {
+    music.play(music.stringPlayable("C5 B A G F E D C ", 300), music.PlaybackMode.UntilDone)
+}
+input.onButtonPressed(Button.B, function () {
+    game.pause()
+    basic.showLeds(`
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+        `)
+    if (4 == sprite.get(LedSpriteProperty.X)) {
+        game.addScore(1)
+        basic.showIcon(IconNames.Skull)
+        playScoreSong()
+    } else {
+        playMissSong()
+    }
+    game.resume()
+})
+function playScoreSong () {
+    music.play(music.stringPlayable("C D E F G A B C5 ", 300), music.PlaybackMode.UntilDone)
+}
+let just_scored = 0
+let sprite: game.LedSprite = null
+let _true = 0
+let _false = 0
+_false = 0
+_true = 1
+basic.showIcon(IconNames.Happy)
+sprite = game.createSprite(2, 2)
+sprite.turn(Direction.Right, 0)
+just_scored = _false
+let list = [0, 2]
+game.setScore(0)
+basic.forever(function () {
+    if (!(game.isPaused())) {
+        sprite.move(1)
+        just_scored = _false
+        basic.pause(200)
+        if (sprite.isTouchingEdge()) {
+            sprite.turn(Direction.Right, 180)
+        }
+    }
+})
